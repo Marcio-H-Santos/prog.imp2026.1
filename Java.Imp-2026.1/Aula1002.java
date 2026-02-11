61500d34984a0d163b69511d969eab8cc0eb988b
Marcio-H-Santos <marcio.00000860052@unicap.br>
2026-02-11 00:23:57 +0000

    Criei o "Olá, turma!"

---
+++ Java.Imp-2026.1/Aula1002.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Aula1002 {
+
+    public static Scanner dado = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Olá, turma!");
+    }
+}
